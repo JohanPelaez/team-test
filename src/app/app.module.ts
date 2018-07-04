fa3849aa07bd63fff369e32486bd43c8799bd51b
Johan Peláez <japaneva@gmail.com>
2018-07-04 11:07:22 -0500

    Initializing the Team project test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchUsersComponent } from './users/search-users/search-users.component';
+import { ViewUsersComponent } from './users/view-users/view-users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchUsersComponent,
+    ViewUsersComponent
   ],
   imports: [
     BrowserModule
